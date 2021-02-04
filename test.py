a3316e6913c44bcff160a8e9276871e4c15b0c6a
Jonathan Leeming <jpleeming51@gmail.com>
2021-02-04 15:36:09 +0000

    Changed log file location

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from os.path import abspath, dirname, join
 from html_renderer.parser import Parser
 from html_renderer.attributes import Class
 from html_renderer.selector import selector
@@ -6,14 +7,16 @@ from html_renderer.selector import selector
 # p.parse('templates', 'includes', 'header.shtml')
 # log(p.result)
 
+log_file = join(dirname(abspath(__file__)), 'log.txt')
+
 def log(*msg, sep: str = ' ', method = str):
     with open('out.txt', 'a') as f:
         f.write(sep.join(method(m) for m in msg) + '\n')
 
 
 if __name__ == '__main':
-    with open('out.txt', 'w') as f:
-        f.write('')
+    with open('out.txt', 'a') as f:
+        f.write('=============== RESTART ===============')
     
     c = Class()
     c += 'header'
