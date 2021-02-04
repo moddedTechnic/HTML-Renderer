29d5a64b663e6b81f92b45438db905a6ec06d811
Jonathan Leeming <jpleeming51@gmail.com>
2021-02-04 16:01:08 +0000

    Converted test to unittest

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
-from os.path import abspath, dirname, join
-from html_renderer.parser import Parser
-from html_renderer.attributes import Class
 from html_renderer.selector import selector
+from os.path import abspath, dirname, join
+from unittest import TestCase, main
+
+# from html_renderer.parser import Parser
+from html_renderer.attributes import Class
+
+# from html_renderer.selector import selector
 
 log_file = join(dirname(abspath(__file__)), 'log.txt')
 
@@ -10,18 +14,27 @@ def log(*msg, sep: str = ' ', method = str):
         f.write(sep.join(method(m) for m in msg) + '\n')
 
 
-if __name__ == '__main__':
-    with open(log_file, 'a') as f:
-        f.write('=============== RESTART ===============\n')
+with open(log_file, 'a') as f:
+    f.write('\n\n=============== RESTART ===============\n')
     
-    # p = Parser()
-    # p.parse('templates', 'includes', 'header.shtml')
-    # log(p.result)
+# p = Parser()
+# p.parse('templates', 'includes', 'header.shtml')
+# log(p.result)
 
-    c = Class()
-    c += 'header'
-    c += Class() + 'banner'
 
-    log(c, method=repr)
-    log(c)
-    log(selector(c))
+class TestRenderer(TestCase):
+    def test_Class(self):
+        log('test_Class')
+        c = Class()
+        self.assertTrue(isinstance(c.value, set))
+        self.assertEqual(len(c), 0)
+        c += 'header'
+        self.assertEqual(len(c), 1)
+        c += Class() + 'banner'
+        self.assertEqual(len(c), 2)
+        self.assertEqual(c, 'header banner')
+        log(c, method=repr)
+        log(c)
+        log(c, method=selector)
+
+main()
